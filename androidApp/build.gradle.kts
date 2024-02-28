89238c91207fff0d9ee0b4ce7a98e62d3424ca2a
Yeechaan <yechanharry@gmail.com>
2024-02-28 23:56:06 +0900

    feature/upload-app-internal-version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.lee.remember.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1.0"
     }
     buildFeatures {
         compose = true
