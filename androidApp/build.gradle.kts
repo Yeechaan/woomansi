0c75911248a5537c6cfa114eebdc9ba7b22e1b0d
Yeechaan <yechanharry@gmail.com>
2024-03-29 21:15:15 +0900

    bugfix/compose textfield not working after system key back

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -43,11 +43,11 @@ android {
 
 dependencies {
     implementation(project(":shared"))
-    implementation("androidx.compose.ui:ui:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
-    implementation("androidx.compose.foundation:foundation:1.4.3")
-    implementation("androidx.compose.material:material:1.4.3")
+    implementation("androidx.compose.ui:ui:1.6.4")
+    implementation("androidx.compose.ui:ui-tooling:1.6.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.4")
+    implementation("androidx.compose.foundation:foundation:1.6.4")
+    implementation("androidx.compose.material:material:1.6.4")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
 //    implementation("com.google.android.material:material:1.5.0")
